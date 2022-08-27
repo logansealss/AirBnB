e1185a0bc4cd5104f8a0bdda655965ab2b38e3d4
Richard Logan Seals <richard.logan.seals@gmail.com>
2022-08-27 10:18:23 -0500

    added Booking migration/model & User<->Spot association

--- backend/db/models/spot.js
+++ backend/db/models/spot.js
@@ -11,10 +11,20 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      Spot.belongsTo(models.User, {
+        foreignKey: 'ownerId'
+      });
+
       Spot.hasMany(models.SpotImage, {
         foreignKey: 'spotId',
         onDelete: 'CASCADE',
         hooks: true
+      });
+
+      Spot.belongsToMany(models.User, {
+        through: models.Booking,
+        foreignKey: 'spotId',
+        otherKey: 'userId'
       });
     }
   }
