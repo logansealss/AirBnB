a5611b210ac018440b2f73448fbf231c48798fbd
Richard Logan Seals <richard.logan.seals@gmail.com>
2022-08-27 10:40:12 -0500

    added ReviewImages migration/model & associations w/ review

--- backend/db/models/reviewimage.js
+++ backend/db/models/reviewimage.js
@@ -3,7 +3,7 @@ const {
   Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class Review extends Model {
+  class ReviewImage extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
@@ -11,38 +11,24 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      Review.hasMany(models.ReviewImage, {
-        foreignKey: 'reviewId',
-        onDelete: 'CASCADE',
-        hooks: true
+      ReviewImage.belongsTo(models.Review, {
+        foreignKey: 'reviewId'
       });
     }
   }
-  Review.init({
-    spotId: {
+  ReviewImage.init({
+    reviewId: {
       type: DataTypes.INTEGER,
       allowNull: false,
       onDelete: 'cascade'
     },
-    userId: {
-      type: DataTypes.INTEGER,
-      allowNull: false
-    },
-    review: {
+    url: {
       type: DataTypes.STRING,
       allowNull: false
-    },
-    stars: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      validate: {
-        min: 1,
-        max: 5
-      }
     }
   }, {
     sequelize,
-    modelName: 'Review',
+    modelName: 'ReviewImage',
   });
-  return Review;
+  return ReviewImage;
 };
