f6ee1a1bf8857062744367f7e620bb5fbb5b78e8
Richard Logan Seals <richard.logan.seals@gmail.com>
2022-09-20 13:19:07 -0500

    continuing css for individual spot page

--- frontend/src/components/SpotPage/index.js
+++ frontend/src/components/SpotPage/index.js
@@ -1,4 +1,4 @@
-import {useParams, useHistory} from "react-router-dom";
+import { useParams, useHistory } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchSingleSpot, deleteSpot } from "../../store/spotReducer";
@@ -6,7 +6,7 @@ import { fetchReviewsForSpot } from "../../store/reviewsReducer";
 
 import "./SpotPage.css"
 
-function SpotPage(){
+function SpotPage() {
 
     const params = useParams();
     const history = useHistory();
@@ -17,12 +17,12 @@ function SpotPage(){
     const user = useSelector(state => state.session.user);
 
     let loggedInUserIsSpotOwner = false;
-    if(user && user.id === spot.ownerId){
+    if (user && user.id === spot.ownerId) {
         loggedInUserIsSpotOwner = true;
     }
 
     let spotLoaded = false;
-    if(spotId === spot.id){
+    if (spotId === spot.id) {
         spotLoaded = true;
     }
 
@@ -31,39 +31,43 @@ function SpotPage(){
         dispatch(fetchReviewsForSpot(spotId));
     }, [dispatch, spotId]);
 
-    async function deleteSpotClickEvent(){
+    async function deleteSpotClickEvent() {
         await dispatch(deleteSpot(spot.id));
         history.push("/");
     }
 
-    if(Object.keys(spot).length === 0) return null;
+    if (Object.keys(spot).length === 0) return null;
 
     let spotImages = [...spot.SpotImages];
     let previewImageIndex = spotImages.findIndex(image => image.preview === true);
     let previewImage;
-    if(previewImageIndex < 0){
+    if (previewImageIndex < 0) {
         previewImage = spotImages[0];
-    }else{
+    } else {
         previewImage = spotImages[previewImageIndex];
         spotImages.splice(previewImageIndex, 1);
     }
 
-    if(!spotLoaded){
+    if (!spotLoaded) {
         return null;
     }
 
     const reviewValues = Object.values(reviews);
     let reviewsLoaded = false;
-    if(reviewValues.length === 0 || reviewValues[0].spotId === spot.id){
+    if (reviewValues.length === 0 || reviewValues[0].spotId === spot.id) {
         reviewsLoaded = true;
     }
 
     return (
-        <div>
-            Spot Page {spotId}
+        <div className="spot-body-container">
+            <div>
                 <div>
-                    <div>
-                        {spot.name}
+                    <div className="spot-name-container">
+                        <span>
+                            <h1>
+                                {spot.name}
+                            </h1>
+                        </span>
                     </div>
                     <div className="spot-stats">
                         <div>
@@ -72,41 +76,51 @@ function SpotPage(){
                         <div>
                             {spot.avgStarRating === null ? "New" : `${spot.avgStarRating}`}
                         </div>
+                        <div>·</div>
+                        <div>{reviewValues.length} {reviewValues.length === 1 ? "review" : "reviews"}</div>
+                        <div>·</div>
+                        <div>{`${spot.city}, ${spot.state}, ${spot.country}`}</div>
                     </div>
-                    <div className="picture-container">
-                        <div className="preview-image"> 
-                            {previewImage !== undefined ? 
-                                (<img src={previewImage.url}/>) : 
+                </div>
+                <div className="pictures-container">
+                    <div className="single-picture-container">
+                        <div className="preview-image">
+                            {previewImage !== undefined ?
+                                (<img src={previewImage.url} />) :
                                 (<i className="fa-solid fa-image"></i>)
                             }
                         </div>
-                        <div>
+                        <div className="image-column-2">
+
+                        </div>
+                        <div className="image-column-3">
 
                         </div>
                     </div>
-                    <div>
-                        {spot.description}
-                    </div>
-                    <div>
-                        {spot.price}
-                    </div>
-                    {loggedInUserIsSpotOwner && (
-                        <>
-                            <button onClick={deleteSpotClickEvent}>Delete spot</button>
-                            <button onClick={() => history.push(`/updatespot/${spot.id}`)}>Update spot</button>
-                        </>
-                    )}
-                    {reviewsLoaded && (
-                        <>
-                            {reviewValues.map(review => (
-                                <div>
-                                    {review.review}
-                                </div>
-                            ))}
-                        </>
-                    )
-                    }
                 </div>
+                <div>
+                    {spot.description}
+                </div>
+                <div>
+                    {spot.price}
+                </div>
+                {loggedInUserIsSpotOwner && (
+                    <>
+                        <button onClick={deleteSpotClickEvent}>Delete spot</button>
+                        <button onClick={() => history.push(`/updatespot/${spot.id}`)}>Update spot</button>
+                    </>
+                )}
+                {reviewsLoaded && (
+                    <>
+                        {reviewValues.map(review => (
+                            <div>
+                                {review.review}
+                            </div>
+                        ))}
+                    </>
+                )
+                }
+            </div>
         </div>
     );
 }
